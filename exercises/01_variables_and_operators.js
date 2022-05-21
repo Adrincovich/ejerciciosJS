47addbb5bc1371420b035de475f2e609b513c46b
Adrian Drincovich <Adrincovich@irsacorp.com.ar>
2022-05-21 19:39:15 -0300

    Variables y Operadores

---
+++ exercises/01_variables_and_operators.js
@@ -0,0 +1,15 @@
+// Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable. (utilizar concat)
+
+var nombre = "Adrian";
+var apellido = "Drincovich";
+var nombreCompleto = nombre.concat(" ", apellido);
+document.write("Nombre Completo: ", nombreCompleto);
+
+
+/* Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string)
+guardando el resultado de la suma en una 3er variable (utilizar length). */
+
+var texto1 = "Texto1";
+var texto2 = "Texto2";
+var lenghtString = texto1.concat(texto2);
+document.write("  Cantidad de letras en el string: ", lenghtString.length);
